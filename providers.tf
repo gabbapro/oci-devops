926c7ea71251da6d0502c77255ed6b01dcab5abd
Gabriele Provinciali <gabbapro@users.noreply.github.com>
2022-08-31 12:33:23 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -27,16 +27,19 @@ terraform {
 provider "oci" {
             tenancy-ocid = var.tenancy-ocid
             region       = var.region
+  
 }
 
 provider "oci" {
             alias        = "home-region"
             tenancy-ocid = var.tenancy-ocid
             region       = lookup(data.oci-identity-regions.home-region.regions[0], "name")
+  
 }
 
 provider "oci" {
             alias        = "current-region"
             tenancy-ocid = var.tenancy-ocid
             region       = var.region
+  
 }
